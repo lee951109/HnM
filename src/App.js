9c5e7bc173731343b7ffad1ab48d18b1670e7555
lee951109 <sk985326@naver.com>
2022-07-08 15:34:01 +0900

    creat navBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import ProductAll from "./page/ProductAll";
 import Login from "./page/Login";
 import ProductDetail from "./page/ProductDetail";
+import Navbar from "./component/Navbar";
 
 function App() {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<ProductAll />} />
         <Route path="/login" element={<Login />} />
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser } from "@fortawesome/free-regular-svg-icons";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
+const Navbar = () => {
+  const menuList = [
+    "여성",
+    "Divided",
+    "남성",
+    "신생아/유아",
+    "아동",
+    "H&M Home",
+    "Sale",
+    "지속가능성",
+  ];
+  return (
+    <div>
+      <div>
+        <div className="login-button">
+          <FontAwesomeIcon icon={faUser} />
+          <div className="login-text">Login</div>
+        </div>
+      </div>
+      <div className="nav-logo">
+        <img
+          width={200}
+          src="https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/3e17273b-330d-4466-b158-d302aaa27d43"
+        />
+      </div>
+      <div className="menu-area">
+        <ul className="menu-list">
+          {menuList.map((menu, index) => (
+            <li key={index}>{menu}</li>
+          ))}
+        </ul>
+
+        <div className="search-box">
+          <FontAwesomeIcon className="search" icon={faSearch} />
+          <input type="text" placeholder="제품 검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
