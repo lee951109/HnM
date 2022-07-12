25250674ef3ea78d3e3bbb568ce8e3fe4a3c84aa
이지현 <lee951109@github.com>
2022-07-12 22:05:53 +0900

    side bar

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-regular-svg-icons";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faSearch, faBars } from "@fortawesome/free-solid-svg-icons";
 import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = ({ auth, setAuth }) => {
@@ -16,10 +16,11 @@ const Navbar = ({ auth, setAuth }) => {
     "지속가능성",
   ];
 
+  const [width, setWidth] = useState(0);
   const navigate = useNavigate();
 
   const goToLogin = () => {
-    if (auth == false) {
+    if (auth === false) {
       navigate("/login");
     } else {
       setAuth(false);
@@ -27,7 +28,7 @@ const Navbar = ({ auth, setAuth }) => {
   };
 
   const search = (e) => {
-    if (e.key == "Enter") {
+    if (e.key === "Enter") {
       let keyword = e.target.value;
       navigate(`/?q=${keyword}`);
     }
@@ -35,14 +36,30 @@ const Navbar = ({ auth, setAuth }) => {
 
   return (
     <div>
+      <div className="side-menu" style={{ width: width }}>
+        <butto className="closebtn" onClick={() => setWidth(0)}>
+          &times;
+        </butto>
+        <div className="side-menu-list" id="menu-list">
+          {menuList.map((menu, index) => (
+            <button key={index}>{menu}</button>
+          ))}
+        </div>
+      </div>
+      <div className="nav-header">
+        <div className="burger-menu hide">
+          <FontAwesomeIcon icon={faBars} onClick={() => setWidth(250)} />
+        </div>
+      </div>
       <div>
         <div className="login-button" onClick={goToLogin}>
           <FontAwesomeIcon size="lg" icon={faUser} />
           <div className="login-text">
-            {auth == false ? "로그인" : "로그아웃"}
+            {auth === false ? "로그인" : "로그아웃"}
           </div>
         </div>
       </div>
+
       <div className="nav-logo">
         <Link to="/">
           <img
@@ -52,7 +69,7 @@ const Navbar = ({ auth, setAuth }) => {
         </Link>
       </div>
       <div className="menu-area">
-        <ul className="menu-list">
+        <ul className="menu">
           {menuList.map((menu, index) => (
             <li key={index}>{menu}</li>
           ))}
